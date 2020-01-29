bfaf0ed8787d0374ad612ce277e4ff99317309a7
Charlie Knoll <charlie.knoll@gmail.com>
2020-01-29 16:28:11 -0700

    Use data scraped from en.vedur.is

--- src/classes/earthquake.js
+++ src/classes/earthquake.js
@@ -1,45 +1,45 @@
 function Earthquake(data) {
-	this.occuredAt = new Date(parseInt(data.date * 1000)).getTime(); // used as a key
+	this.occuredAt = new Date(data.t).getTime(); // used as a key
 	this.latitude = parseFloat(data.lat);
-	this.longitude = parseFloat(data.long);
-	this.depth = parseFloat(data.depth);
-	this.size = parseFloat(data.size);
-	this.verified = data.quality === 99;
+	this.longitude = parseFloat(data.lon);
+	this.depth = parseFloat(data.dep);
+	this.size = parseFloat(data.s);
+	this.verified = true; //data.q === 99;
 
 	var humanReadable = "";
-	humanReadable += data.loc_dist + " ";
-	humanReadable += data.loc_dir + " ";
-	humanReadable += data.loc_name + " ";
+	humanReadable += data.dL + " ";
+	humanReadable += data.dD + " ";
+	humanReadable += data.dR + " ";
 	this.humanReadableLocation = humanReadable;
-	
-	this.quality = data.quality;
+
+	this.quality = parseInt(data.q);
 }
 
 // Checking if gps coordinates are near Bardarbunga
-Earthquake.prototype.isFromBardarbunga = function() {
-	if(this.latitude >= 64.35 && this.latitude <= 65.2) {
-		if(this.longitude >= -18 && this.longitude <= -16) {
+Earthquake.prototype.isFromBardarbunga = function () {
+	if (this.latitude >= 64.35 && this.latitude <= 65.2) {
+		if (this.longitude >= -18 && this.longitude <= -16) {
 			return true;
 		}
 	}
 	return false;
 };
 
-Earthquake.prototype.color = function(now) {
+Earthquake.prototype.color = function (now) {
 	var diff = now - this.occuredAt;
 	var hours = diff / (60 * 60 * 1000);
 	var opacity = 0.75;
 
-	if(hours <= 4) {
+	if (hours <= 4) {
 		return "rgba(255,0,0," + opacity + ")";
 	}
-	else if(hours <= 12) {
+	else if (hours <= 12) {
 		return "rgba(255, 102, 0," + opacity + ")";
 	}
-	else if(hours <= 24) {
+	else if (hours <= 24) {
 		return "rgba(255, 255, 0," + opacity + ")";
 	}
-	else if(hours <= 36) {
+	else if (hours <= 36) {
 		return "rgba(51, 102, 204," + opacity + ")";
 	}
 	else {
@@ -47,20 +47,20 @@ Earthquake.prototype.color = function(now) {
 	}
 };
 
-Earthquake.prototype.colorHex = function(now) {
+Earthquake.prototype.colorHex = function (now) {
 	var diff = now - this.occuredAt;
 	var hours = diff / (60 * 60 * 1000);
 
-	if(hours <= 4) {
+	if (hours <= 4) {
 		return 0xff0000;
 	}
-	else if(hours <= 12) {
+	else if (hours <= 12) {
 		return 0xff6600;
 	}
-	else if(hours <= 24) {
+	else if (hours <= 24) {
 		return 0xffff00;
 	}
-	else if(hours <= 36) {
+	else if (hours <= 36) {
 		return 0x3366cc;
 	}
 	else {
--- src/services/earthquake.js
+++ src/services/earthquake.js
@@ -1,22 +1,22 @@
 app.factory("EarthquakeService", [
     "$http",
-    function($http) {
+    function ($http) {
         return {
-            getEarthquakesLastHours: function(hours, getAllQuakes) {
-                return $http.get("https://earthquakes-is.appspot.com/earthquakes").then(
-                    function(response) {
+            getEarthquakesLastHours: function (hours, getAllQuakes) {
+                return $http.get("http://localhost:3000/url-to-json?value=https://en.vedur.is/earthquakes-and-volcanism/earthquakes&o=VI.quakeInfo").then(
+                    function (response) {
                         var earthquakes = [];
 
-                        if(response.status === 200) {
-                            for(var i = 0; i < response.data.items.length; ++i) {
-                                var currentEarthquakeData = response.data.items[i];
+                        if (response.status === 200) {
+                            for (var i = 0; i < response.data.length; ++i) {
+                                var currentEarthquakeData = response.data[i];
                                 var earthquake = new Earthquake(currentEarthquakeData);
-                                if(earthquake.isFromBardarbunga() || getAllQuakes) {
+                                if (earthquake.isFromBardarbunga() || getAllQuakes) {
                                     earthquakes.push(earthquake);
                                 }
                             }
                         }
-                        
+
                         return earthquakes;
                     }
                 );
